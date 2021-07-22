61f14e85b574eaaf156b3e54f7084b31ce8e5577
emilia-toh <65080653+emilia-toh@users.noreply.github.com>
2021-07-22 22:33:49 +0800

    added lines

--- labsheet/demoVcs.py
+++ labsheet/demoVcs.py
@@ -1,1 +1,12 @@
-print('testing')
+class Calculator:
+    def add(num1,num2):
+        Add = num1 + num2
+        print(Add)
+        return Add
+    def minus(num1,num2):
+        Minus = num1 - num2
+        print(Minus)
+        return Minus
+
+Calculator.add(50,40)
+Calculator.minus(50,40)
