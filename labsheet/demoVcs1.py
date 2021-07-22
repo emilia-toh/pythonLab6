2374a54a4736924488c2afdc6759abb3f5218f75
emilia-toh <65080653+emilia-toh@users.noreply.github.com>
2021-07-22 21:44:57 +0800

    added first 13 line

---
+++ labsheet/demoVcs1.py
@@ -0,0 +1,14 @@
+class Calculator:
+    def add(num1,num2):
+        total = num1 + num2
+        print(total)
+        return total
+
+    def minus(num1,num2):
+        sum = num1 - num2
+        print(sum)
+        return sum
+
+Calculator.add(50,40)
+
+Calculator.minus(50,40)
