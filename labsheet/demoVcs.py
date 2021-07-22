6cd16bc9d1e16415e4a9504648d3d846f8690d48
emilia-toh <65080653+emilia-toh@users.noreply.github.com>
2021-07-22 22:42:21 +0800

    added new lines

--- labsheet/demoVcs.py
+++ labsheet/demoVcs.py
@@ -7,6 +7,11 @@ class Calculator:
         Minus = num1 - num2
         print(Minus)
         return Minus
+    def multiply(num1,num2):
+        Multiply = num1 * num2
+        print(Multiply)
+        return Multiply
 
 Calculator.add(50,40)
 Calculator.minus(50,40)
+Calculator.multiply(50,40)
